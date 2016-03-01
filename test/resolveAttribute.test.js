ecd48ffecbdbb409fe8d59d0d401e300cb50e3eb
Evan Krambuhl <thekrambuhl@gmail.com>
2016-02-29 22:22:03 -0800

    resolve attribute test

---
+++ test/resolveAttribute.test.js
@@ -0,0 +1,65 @@
+var config = require('./_config.js');
+var test = require('tape');
+
+var resolveAttribute = require('../dist/resolveAttribute');
+
+const defTree = {
+  type: 'attr',
+  name: 'default',
+  data: 'value'
+};
+
+test('resolveAttribute(tree, props, config) :: name/data strings', function(t) {
+  t.plan(3);
+
+  var tree = Object.assign({}, defTree);
+  var res = resolveAttribute(tree, { min: 10, max: 90 }, config);
+
+  t.equal(res.type, 'attr');
+  t.equal(res.name, 'default');
+  t.equal(res.data, 'value');
+});
+
+test('resolveAttribute(tree, props, config) :: name array', function(t) {
+  t.plan(1);
+
+  var tree = Object.assign({}, defTree, { name: [
+    { type: 'variable', path: 'min' },
+    { type: 'text', data: ' - ' },
+    { type: 'variable', path: 'max' },
+  ]});
+  
+  var res = resolveAttribute(tree, { min: 10, max: 90 }, config);
+
+  t.equal(res.name, '10 - 90');
+});
+
+test('resolveAttribute(tree, props, config) :: data array (name not data)', function(t) {
+  t.plan(1);
+
+  var tree = Object.assign({}, defTree, { data: [
+    { type: 'variable', path: 'min' },
+    { type: 'text', data: ' - ' },
+    { type: 'variable', path: 'max' },
+  ]});
+  
+  var res = resolveAttribute(tree, { min: 10, max: 90 }, config);
+
+  t.equal(res.data, '10 - 90');
+});
+
+
+test('resolveAttribute(tree, props, config) :: data array (name is data)', function(t) {
+  t.plan(3);
+
+  var tree = Object.assign({}, defTree, { 
+    name: 'data',
+    data: [{ type: 'variable', path: 'set' }]
+  });
+  
+  var res = resolveAttribute(tree, { set: [1,2,3] }, config);
+
+  t.equal(res.data[0], 1);
+  t.equal(res.data[1], 2);
+  t.equal(res.data[2], 3);
+});
