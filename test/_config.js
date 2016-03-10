b1cdae1db3e887c2b88193825578875da58b3db1
Evan Krambuhl <thekrambuhl@gmail.com>
2016-03-10 00:10:06 -0800

    success!

--- test/_config.js
+++ test/_config.js
@@ -3,7 +3,6 @@ const Config = require('rogain-config');
 var config = new Config();
 
 config.registerComponent(require('rogain-core-helpers'));
-
 config.registerComponent({
   Test: require('./fixtures/Test.json'),
   TestBlock: require('./fixtures/TestBlock.json'),
--- test/resolveTree.test.js
+++ test/resolveTree.test.js
@@ -2,24 +2,84 @@ var config = require('./_config');
 var test = require('tape');
 var resolveTree = require('../dist/resolveTree');
 
+var red = { hex: 'f00', slug: 'red' };
+var blue = { hex: '00f', slug: 'blue' };
+var data = {
+  data: 'data',
+  tag: 'section',
+  colors: [red, blue],
+  scripts: {
+    main: '/scripts/main.js'
+  }
+}
 
-test('resolveTree', function(t) {
-  t.plan(2);
+test('resolveTree', function(t, d) {
+  var res = resolveTree(config.components.get('Test'), data, config);
 
-  var res = resolveTree(config.components.get('Test'), {
-    data: 'data',
-    tag: 'section',
-    colors: [
-      { hex: 'f00', slug: 'red' },
-      { hex: '00f', slug: 'blue' }
-    ],
-    scripts: {
-      main: '/scripts/main.js'
-    }
-  }, config);
+  t.plan(43);
 
+  // 2
   t.equal(res.type, 'tag');
   t.equal(res.name, 'div');
 
-  console.log(JSON.stringify(res, null, 2))
+  // 5
+  t.equal(res.children[0].type, 'tag');
+  t.equal(res.children[0].name, 'section');
+  t.equal(res.children[0].attrs.class, 'block');
+  t.equal(res.children[0].attrs['data-hex'], '#f00');
+  t.equal(res.children[0].children, 'red');
+
+  // 4
+  t.equal(res.children[1].type, 'tag');
+  t.equal(res.children[1].name, 'div');
+  t.equal(res.children[1].attrs.id, 'colors');
+  t.equal(res.children[1].attrs.class, 'component box');
+
+  // 5
+  t.equal(res.children[1].children[0].type, 'tag');
+  t.equal(res.children[1].children[0].name, 'div');
+  t.equal(res.children[1].children[0].attrs.class, 'block');
+  t.equal(res.children[1].children[0].attrs['data-hex'], '#00f');
+  t.equal(res.children[1].children[0].children, 'blue');
+
+  // 4
+  t.equal(res.children[1].children[1].type, 'tag');
+  t.equal(res.children[1].children[1].name, 'a');
+  t.equal(res.children[1].children[1].attrs.href, '/colors');
+  t.equal(res.children[1].children[1].children, 'Read More');
+
+  // 1
+  t.equal(res.children[2].length, 2);
+
+  // 5
+  t.equal(res.children[2][0][0].type, 'tag');
+  t.equal(res.children[2][0][0].name, 'div');
+  t.equal(res.children[2][0][0].attrs.class, 'block');
+  t.equal(res.children[2][0][0].attrs['data-hex'], '#f00');
+  t.equal(res.children[2][0][0].children, 'red0');
+
+  // 4
+  t.equal(res.children[2][0][1].type, 'tag');
+  t.equal(res.children[2][0][1].name, 'a');
+  t.equal(res.children[2][0][1].attrs.href, '/colors/red');
+  t.equal(res.children[2][0][1].children, 'Read More');
+
+  // 5
+  t.equal(res.children[2][1][0].type, 'tag');
+  t.equal(res.children[2][1][0].name, 'div');
+  t.equal(res.children[2][1][0].attrs.class, 'block');
+  t.equal(res.children[2][1][0].attrs['data-hex'], '#00f');
+  t.equal(res.children[2][1][0].children, 'blue1');
+
+  // 4
+  t.equal(res.children[2][1][1].type, 'tag');
+  t.equal(res.children[2][1][1].name, 'a');
+  t.equal(res.children[2][1][1].attrs.href, '/colors/blue');
+  t.equal(res.children[2][1][1].children, 'Read More');
+
+  // 4
+  t.equal(res.children[3].type, 'tag');
+  t.equal(res.children[3].name, 'script');
+  t.equal(res.children[3].attrs.src, '/scripts/main.js');
+  t.equal(res.children[3].children, 'function hello(str) { }');
 });
